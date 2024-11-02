ba76375b4ec1859b375b0b5f21ef7e1e7c567c46
tiagolimar <tiagolima.r2@gmail.com>
2024-11-02 08:06:04 -0300

    create a select

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import Campo from './components/Campo.jsx';
 
 function App() {
+    const [variant, setVariant] = useState('primary');
+
+    const handleChange = (event) => {
+        const newVariant = event.target.value;
+        setVariant(newVariant);
+    }
+
     return (
         <>
-            <form className="container border border-dark rounded shadow-sm mt-4 d-flex flex-column gap-4 p-4">
-                <h1 className="text-center">Formulário</h1>
+            <form className={`container border border-${variant} rounded shadow-sm mt-4 d-flex flex-column gap-4 p-4`}>
+                <h1 className={`text-center text-${variant}`}>Formulário</h1>
                 
-                <Campo id="nome" label="Nome" />
-                <Campo id="email" label="Email" type="email" />
-                <Campo id="senha" label="Senha" type="password" />
+                <div className="form-group w-25 mx-auto">
+                    <label htmlFor="cor" className={`text-${variant}`}>Selecione a cor:</label>
+                    <select id="cor" className={`form-select border border-${variant}`} onChange={handleChange}>
+                        <option value="primary">Azul</option>
+                        <option value="secondary">Cinza</option>
+                        <option value="success">Verde</option>
+                        <option value="danger">Vermelho</option>
+                        <option value="warning">Amarelo</option>
+                        <option value="info">Azul Claro</option>
+                        <option value="light">Cinza Claro</option>
+                        <option value="dark">Preto</option>
+                    </select>
+                </div>
+
+                <Campo variant={variant} id="nome" label="Nome" />
+                <Campo variant={variant} id="email" label="Email" type="email" />
+                <Campo variant={variant} id="senha" label="Senha" type="password" />
                 
-                <button className="btn btn-primary shadow-sm mb-4">Cadastrar</button>
+                <button className={`btn btn-${variant} shadow-sm mb-4`}>Cadastrar</button>
 
             </form>
         </>
--- src/components/Campo.jsx
+++ src/components/Campo.jsx
@@ -1,15 +1,15 @@
-export default function Campo({label, id, type = "text"}) {
+export default function Campo({variant, label, id, type = "text"}) {
     return (
         <div className="form-group">
             <label htmlFor={id} 
-                className="form-label text-primary">
+                className={`form-label text-${variant}`}>
                 {label}:
             </label>
             <input
                 type={type}
                 id={id}
                 name={id}
-                className="form-control border border-primary shadow-sm"
+                className={`form-control border border-${variant} shadow-sm`}
             />
         </div>
     );
